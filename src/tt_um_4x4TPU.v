9913eff2a27f1c520bb772fd9c9b660cde0bdab6
Jiya <jiya.nair@mail.utoronto.ca>
2026-02-20 12:08:22 -0500

    Update tt_um_4x4TPU.v

--- src/tt_um_4x4TPU.v
+++ src/tt_um_4x4TPU.v
@@ -19,7 +19,7 @@ module tt_um_4x4TPU (
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
-  assign uio_oe  = 8'hF0; // top 4 bits are outputs, bottom 4 are inputs
+  assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, 1'b0};
