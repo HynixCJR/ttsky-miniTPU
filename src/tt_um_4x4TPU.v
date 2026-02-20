2fbc09ac0884ccc8e2829c5c2ab48e5704ca294a
Jiya <jiya.nair@mail.utoronto.ca>
2026-02-20 12:05:58 -0500

    Changed title to match guidelines, updated info files
    The testing section in info.md is not great right now, but will update as we wrap up testing.

--- src/tt_um_4x4TPU.v
+++ src/tt_um_4x4TPU.v
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2026 Albert Huang, Matthew Kong, Jiya Nair, Rivera Wijaya
  * SPDX-License-Identifier: Apache-2.0
  */
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_4x4TPU (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -19,9 +19,9 @@ module tt_um_example (
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
-  assign uio_oe  = 0;
+  assign uio_oe  = 8'hF0; // top 4 bits are outputs, bottom 4 are inputs
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, 1'b0};
 
 endmodule
